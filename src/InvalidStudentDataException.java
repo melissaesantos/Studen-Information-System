2a6225101788dde05344b0ff57ad0913876c3be0
Melissa Santos <melissasantos2202@gmail.com>
2024-05-26 21:04:09 -0700

    Added customexception file

---
+++ src/InvalidStudentDataException.java
@@ -0,0 +1,3 @@
+public class InvalidStudentDataException extends Exception {
+    
+}
--- src/StudentRecord.java
+++ src/StudentRecord.java
@@ -1,3 +1,6 @@
-public class StudentRecord extends Student{
-    
+import java.util.HashMap;
+import java.util.Map;
+
+public class StudentRecord{
+   private Map<String, Student> students = new HashMap<>();
 }
