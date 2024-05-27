94068e7bac81b79af2b05116141f1d0e65af3a53
Melissa Santos <melissasantos2202@gmail.com>
2024-05-26 21:41:07 -0700

    added more methods to studentrecord.java

--- src/StudentRecord.java
+++ src/StudentRecord.java
@@ -1,5 +1,7 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.List;
+import java.util.ArrayList;
 
 public class StudentRecord{
    private Map<String, Student> students = new HashMap<>();
@@ -16,5 +18,15 @@ public class StudentRecord{
             throw new StudentNotFoundException("Student record does not exist.");
         }
         students.remove(studentID);
+   }
+   public Student getstudent(String studentID) throws StudentNotFoundException{
+        if(!students.containsKey(studentID)){
+            throw new StudentNotFoundException("Student not found.");
+        }
+        return students.get(studentID);
+   }
+   public List<Student> getAllStudents(){
+        return new ArrayList<>(students.values());
+
    }
 }
