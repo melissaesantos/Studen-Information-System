27a228daf03991d517f426682ad20206ae8975b3
Melissa Santos <melissasantos2202@gmail.com>
2024-05-26 21:29:03 -0700

    remove student code

--- src/StudentRecord.java
+++ src/StudentRecord.java
@@ -4,10 +4,17 @@ import java.util.Map;
 public class StudentRecord{
    private Map<String, Student> students = new HashMap<>();
 
-   public void addStudent(Student student){
+   public void addStudent(Student student) throws InvalidStudentDataException{
         if( student == null||student.getName() == null || student.getstudentID() == null || student.getAge()<=0|| student.getMajor() == null ){
-            throw new InvalidStudentDataException("Invalid student data.")
+            throw new InvalidStudentDataException("Invalid student data.");
         }
         students.put(student.getstudentID(),student);
+   }
+
+   public void removeStudent(String studentID) throws StudentNotFoundException{
+        if (students.containsKey(studentID) == false){
+            throw new StudentNotFoundException("Student record does not exist.");
+        }
+        students.remove(studentID);
    }
 }
