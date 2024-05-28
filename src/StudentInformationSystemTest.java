1bc40c1f037bb9dab4bf14a1dd6b1a5eb4909564
Melissa Santos <melissasantos2202@gmail.com>
2024-05-28 09:13:56 -0700

    testing

--- src/StudentInformationSystemTest.java
+++ src/StudentInformationSystemTest.java
@@ -1,5 +1,12 @@
+package test;
 import org.junit.Before;
 import org.junit.Test;
+
+import InvalidStudentDataException;
+import Student;
+import StudentNotFoundException;
+import StudentRecord;
+
 import static org.junit.Assert.*;
 
 
@@ -8,6 +15,18 @@ public class StudentInformationSystemTest {
     private Student student1;
     private Student student2;
     
+    @Before
+    public void setUp() {
+        studentRecord = new StudentRecord();
+        student1 = new Student("S001", "John Doe", 20, "Computer Science");
+        student2 = new Student("S002", "Jane Smith", 22, "Mathematics");
+    }
 
-    
+    @Test
+    public void testAddStudent() throws InvalidStudentDataException, StudentNotFoundException {
+        studentRecord.addStudent(student1);
+        assertEquals(student1, studentRecord.getstudent("S001"));
+    }
+
+
 }
